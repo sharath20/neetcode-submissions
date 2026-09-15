f7f35d70e2c692e004de3abdb4da2f0db79a4764
Sharath Mudhigonda <mvcsharathsai95@gmail.com>
2026-09-14 20:04:45 -0500

    Add: python-double-ended-queue - submission-0

---
+++ Python Coding Interviews/python-double-ended-queue/submission-0.py
@@ -0,0 +1,20 @@
+from typing import List, Deque
+from collections import deque
+
+
+def rotate_list(arr: List[int], k: int) -> Deque[int]:
+    res = deque(arr)
+    while k > 0:
+        right_element = res.pop()
+        res.appendleft(right_element)
+        k -= 1
+    return res
+
+
+# do not modify below this line
+print(rotate_list([1, 2, 3, 4, 5], 0))
+print(rotate_list([1, 2, 3, 4, 5], 1))
+print(rotate_list([1, 2, 3, 4, 5], 2))
+print(rotate_list([1, 2, 3, 4, 5], 3))
+print(rotate_list([1, 2, 3, 4, 5], 4))
+print(rotate_list([1, 2, 3, 4, 5], 5))
